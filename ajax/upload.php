cde1af8035bd2243cf850a0d5ece17b66ff4a375
Gabriel Martins <martinsbiel@hotmail.com>
2021-03-11 22:13:07 -0300

    Indentation fixed

--- ajax/upload.php
+++ ajax/upload.php
@@ -1,30 +1,30 @@
 <?php
-	require_once '../config.php';
-	require_once '../classes/File.php';
+    require_once '../config.php';
+    require_once '../classes/File.php';
 
-	// check if the file is set
-	$file = (isset($_FILES['file'])) ? $_FILES['file'] : null;
-	$fileName = (isset($_FILES['file'])) ? $_FILES['file']['name'] : null;
-	$fileTempname = (isset($_FILES['file'])) ? $_FILES['file']['tmp_name'] : null;
-	$fileSize = (isset($_FILES['file'])) ? $_FILES['file']['size'] : null;
+    // check if the file is set
+    $file = (isset($_FILES['file'])) ? $_FILES['file'] : null;
+    $fileName = (isset($_FILES['file'])) ? $_FILES['file']['name'] : null;
+    $fileTempname = (isset($_FILES['file'])) ? $_FILES['file']['tmp_name'] : null;
+    $fileSize = (isset($_FILES['file'])) ? $_FILES['file']['size'] : null;
 
-	$upload = new File();
+    $upload = new File();
 
-	if(!empty($fileName)){
-		if($upload->checkExtension($upload->getFileExtension($fileName), $allowedExtensions)){
-			echo '<div class="alert alert-danger" role="alert">Extension not allowed.</div>';
-		}elseif($upload->checkSize($fileSize, $allowedSize)){
-			echo '<div class="alert alert-danger" role="alert">The file is too big.</div>';
-		} else {
-			$newFileName = $upload->changeFileName($upload->getFileExtension($fileName));
+    if(!empty($fileName)){
+        if($upload->checkExtension($upload->getFileExtension($fileName), $allowedExtensions)){
+            echo '<div class="alert alert-danger" role="alert">Extension not allowed.</div>';
+        }elseif($upload->checkSize($fileSize, $allowedSize)){
+            echo '<div class="alert alert-danger" role="alert">The file is too big.</div>';
+        } else {
+            $newFileName = $upload->changeFileName($upload->getFileExtension($fileName));
 
-			$upload->uploadFile($fileTempname, $newFileName, $folder);
+            $upload->uploadFile($fileTempname, $newFileName, $folder);
 
-			echo '<div class="input-group">
-  					<input id="copylink-target" type="text" class="form-control" value="' . $_SERVER['SERVER_NAME'] . '/u/' . $newFileName . '" readonly>
-  					<button data-clipboard-target="#copylink-target" class="btn btn-secondary copylink" type="button">Copy</button>
-				</div>';
-		}
-	} else {
-		echo '<div class="alert alert-danger" role="alert">Error, select a file.</div>';
-	}
+            echo '<div class="input-group">
+                    <input id="copylink-target" type="text" class="form-control" value="' . $_SERVER['SERVER_NAME'] . '/u/' . $newFileName . '" readonly>
+                    <button data-clipboard-target="#copylink-target" class="btn btn-secondary copylink" type="button">Copy</button>
+                </div>';
+        }
+    } else {
+        echo '<div class="alert alert-danger" role="alert">Error, select a file.</div>';
+    }
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,34 +1,34 @@
 $(document).ready(() => {
-	let bar = $('.progress');
-	let progress = $('.progress-bar');
-	let status = $('.return');
+    let bar = $('.progress');
+    let progress = $('.progress-bar');
+    let status = $('.return');
 
-	$('#upload').ajaxForm({
-		url: '/ajax/upload.php',
-		beforeSend: () => {
-			bar.show();
-			status.empty();
-			let percentVal = '0%';
-			progress.width(percentVal);
-		},
-		uploadProgress: (event, position, total, percentComplete) => {
-			let percentVal = percentComplete + '%';
-			progress.width(percentVal);
-		},
-		success: () => {
-			let percentVal = '100%';
-			progress.width(percentVal);
-		},
-		complete: data => {
-			status.html(data.responseText);
-			bar.hide();
-		}
-	});
+    $('#upload').ajaxForm({
+        url: '/ajax/upload.php',
+        beforeSend: () => {
+            bar.show();
+            status.empty();
+            let percentVal = '0%';
+            progress.width(percentVal);
+        },
+        uploadProgress: (event, position, total, percentComplete) => {
+            let percentVal = percentComplete + '%';
+            progress.width(percentVal);
+        },
+        success: () => {
+            let percentVal = '100%';
+            progress.width(percentVal);
+        },
+        complete: data => {
+            status.html(data.responseText);
+            bar.hide();
+        }
+    });
 
-	// copy to clipboard feature
-	let clipboard = new ClipboardJS('.copylink');
+    // copy to clipboard feature
+    let clipboard = new ClipboardJS('.copylink');
 
-	clipboard.on('success', () => {
-		$('.copylink').html('Copied');
-	});
+    clipboard.on('success', () => {
+        $('.copylink').html('Copied');
+    });
 });
